c847c9f9dcad5ae8f7ff3eeb5dcc3b88c502b36f
Natalie Lane <nlane@udel.edu>
2021-12-09 12:37:47 -0500

    day 9 pt2, clean-up

--- src/puzzles/day9.js
+++ src/puzzles/day9.js
@@ -9,6 +9,26 @@ const isLowPoint = (grid, [i, j]) => {
   return adjNums.every((el) => el > grid[i][j]);
 };
 
+const dfs = (grid, [i, j]) => {
+  if (
+    i > -1 &&
+    j > -1 &&
+    i < grid.length &&
+    j < grid[0].length &&
+    grid[i][j] !== 9
+  ) {
+    grid[i][j] = 9;
+    return (
+      1 +
+      dfs(grid, [i + 1, j]) +
+      dfs(grid, [i, j + 1]) +
+      dfs(grid, [i - 1, j]) +
+      dfs(grid, [i, j - 1])
+    );
+  }
+  return 0;
+};
+
 export default () => {
   const input = read2D("src/inputs/day9.txt");
 
@@ -21,5 +41,21 @@ export default () => {
     }
   }
 
-  console.log(sum);
+  console.log("Part 1: ", sum);
+
+  let basins = [];
+  for (let i = 0; i < input.length; i++) {
+    for (let j = 0; j < input[0].length; j++) {
+      if (input[i][j] !== 9) {
+        basins.push(dfs(input, [i, j]));
+      }
+    }
+  }
+
+  basins = basins.sort((a, b) => {
+    if (a < b) return 1;
+    else return -1;
+  });
+
+  console.log("Part 2: ", basins[0] * basins[1] * basins[2]);
 };
